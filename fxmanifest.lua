87a20dbceeef8fffb5e78facbf644b0378964f6e
xjer2168 <69177114+xjer2168@users.noreply.github.com>
2021-01-31 12:43:50 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 games {'gta5'}
 
-description 'Peacetime Shooting Prevention and Announcement'
-author 'Scott M'
+description 'JRP Peacetime Shooting Prevention and Announcement'
+author 'JRP Development Team'
 version 'v1.2'
 
 shared_scripts {
